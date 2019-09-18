457a8483d68b994e474bb3bac65d261dcac5daf1
Jakub Berthoty <berthotyms@kros.sk>
2019-09-18 14:47:35 +0200

    Toolbar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from './user.model.mock';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'simple-chat';
+export class AppComponent implements OnInit {
+  user: User = null;
+
+  constructor(
+    public router: Router) { }
+
+  ngOnInit() {
+  }
+
+  signout() {
+    this.user = null;
+  }
 }
