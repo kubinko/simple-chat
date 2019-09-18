40f6e4b1a4749b1e85c43471a879e326eec507ae
Jakub Berthoty <berthotyms@kros.sk>
2019-09-18 15:02:09 +0200

    Base page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { User } from './user.model.mock';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,22 @@ import { User } from './user.model.mock';
 })
 export class AppComponent implements OnInit {
   user: User = null;
+  quoteText$: BehaviorSubject<string> = new BehaviorSubject('');
+  quoteAuthor$: BehaviorSubject<string> = new BehaviorSubject('');
 
   constructor(
     public router: Router) { }
 
   ngOnInit() {
+    this.quoteText$.next('People say nothing is impossible, but I do nothing every day.');
+    this.quoteAuthor$.next('A. A. Milne');
+  }
+
+  signin() {
+    this.user = new User();
+    this.user.displayName = 'Jozef Mrkvička';
+    this.user.photoURL =
+      'https://upload.wikimedia.org/wikipedia/commons/1/1e/Trump_protest_in_London182_%2842690728125%29_%28cropped%29.jpg';
   }
 
   signout() {
